4adc12d6583adba398f7cee00b321fd2c74a9f6a
Piotr Araszkiewicz <araszkiewiczpiotr@gmail.com>
2016-10-17 23:23:47 +0200

    first method of auth

--- ZreperujTo.UWP/ViewModels/MainPageViewModel.cs
+++ ZreperujTo.UWP/ViewModels/MainPageViewModel.cs
@@ -2,7 +2,9 @@ using Template10.Mvvm;
 using System.Collections.Generic;
 using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
 using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
 
@@ -43,6 +45,57 @@ namespace ZreperujTo.UWP.ViewModels
         {
             args.Cancel = false;
             await Task.CompletedTask;
+        }
+
+        public async void Authorization()
+        {
+
+            var callback = new Uri(@"https://zreperujto.azurewebsites.net/api/Profile");
+            var startUri = new Uri(@"https://zreperujto.azurewebsites.net/connect/authorize?client_id=mobile&redirect_uri=https://zreperujto.azurewebsites.net/api/Profile&response_type=code id_token token&scope=openid zreperuj_to_api offline_access&nonce=a18800e2-78cc-4757-a79c-542e4d01517f");
+
+            try
+            {
+                var webAuthenticationResult =
+                    await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, startUri, callback);
+
+                if (webAuthenticationResult.ResponseStatus == WebAuthenticationStatus.Success)
+                {
+                    var payload = webAuthenticationResult.ResponseData.Substring(webAuthenticationResult.ResponseData.IndexOf('#') + 1);
+
+                    var parameters = (from parameter in payload.Split('&')
+                                      let pair = parameter.Split('=')
+                                      select new { Name = pair[0], Value = pair[1] })
+                          .ToDictionary(element => element.Name, element => element.Value);
+                    //Claims.Clear();
+                    //foreach (var par in parameters)
+                    //{
+                    //    Claims.Add(new Claim
+                    //    {
+                    //        Type = par.Key,
+                    //        Value = par.Value
+                    //    });
+                    //}
+
+                    string error;
+                    // If an "error" parameter has been added by the authorization server, return an exception.
+                    // Note: the optional "error_description" can be used to determine why the process failed.
+                    if (parameters.TryGetValue("error", out error))
+                    {
+                        //throw new InvalidOperationException("An error occurred during the authorization process.");
+                    }
+
+                    string token;
+                    // Ensure an access token has been returned by the authorization server.
+                    if (!parameters.TryGetValue("access_token", out token))
+                    {
+                        //throw new InvalidOperationException("The access token was missing from the OAuth2 response.");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
 
         public void GotoDetailsPage() =>
