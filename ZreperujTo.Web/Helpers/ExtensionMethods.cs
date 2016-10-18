e125f37721f749f54bf0849605adf1a8a4f599eb
Mateusz Dudek <mateudu@outlook.com>
2016-10-18 15:41:48 +0200

    Improved performance

---
+++ ZreperujTo.Web/Helpers/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZreperujTo.Web.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static string GetSubId(this System.Security.Claims.ClaimsPrincipal user)
+        {
+            return user.Claims.FirstOrDefault(
+                        x => x.Type == @"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+        }
+    }
+}
