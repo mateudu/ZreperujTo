f4e828ff4c6c8d846ce12c65f55afd6369f71fea
piotr <piotr@DESKTOP-K1D8K2P>
2016-10-23 17:13:06 +0200

    finish

--- ZreperujTo.UWP/ViewModels/ReportFailViewModel.cs
+++ ZreperujTo.UWP/ViewModels/ReportFailViewModel.cs
@@ -80,10 +80,10 @@ namespace ZreperujTo.UWP.ViewModels
 
         public object ImageSource
         {
-            get { return _imageSource; }
+            get { return photo; }
             set
             {
-                _imageSource = value;
+                photo = value as StorageFile;
                 RaisePropertyChanged();
             }
         }
